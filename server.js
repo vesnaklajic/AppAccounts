97a4d773aace2972e6d310bc25412ed28dde9235
vesnaklajic <vesnaklajic77@gmail.com>
2021-02-16 23:38:33 +0100

    Added delete and edit button

--- server.js
+++ server.js
@@ -27,8 +27,43 @@ app.post('/save', (req, res) => {
     }, (err, data) => {
         res.redirect('/')
     })
-
 })
+
+
+app.get('/edit', (req, res) => {
+    db.users.find((err, data) => {
+        res.render('edit_view', { data: data })
+    })
+})
+
+
+app.get('/delete_acc/:id', (req, res) => {
+    let id = req.params.id;
+    db.users.remove({ "_id": db.ObjectId(id) }, (err, data) => {
+        res.redirect('/')
+    })
+})
+
+app.get('/edit_acc/:id', (req, res) => {
+    let id = req.params.id;
+    db.users.findOne({ "_id": db.ObjectId(id) }, (err, data) => {
+        res.render('edit-form-view', { data: data })
+    })
+})
+
+app.post('/edit_db', (req, res) => {
+    let id = req.body.id;
+    db.users.update({"_id":db.ObjectId(id)}, {$set: {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            tel: req.body.tel
+        }
+    }, (err, data) => {
+        res.redirect('/')
+    })
+})
+
 app.listen('3000', () => {
     console.log("Listening server on 3000");
 })
