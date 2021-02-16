9ceebff91d0475801553b52908c93eec1d5ffe08
vesnaklajic <vesnaklajic77@gmail.com>
2021-02-16 17:51:57 +0100

    /save Add Account

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
-const express=require('express');
-const mongojs=require ('mongojs');
-const db=mongojs('accounts',['users']);
+const express = require('express');
+const mongojs = require('mongojs');
+const db = mongojs('accounts', ['users']);
 
-const app=express();
+const app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
+//body parser is now in express
+//now we have acces to the body of request /save
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/',(req,res)=>{
-    db.users.find((err,data)=>{
-        res.render('index',{data:data})
-    })   
+app.get('/', (req, res) => {
+    db.users.find((err, data) => {
+        res.render('index', { data: data })
+    })
 })
 
-app.get('/add',(req,res)=>{
+app.get('/add', (req, res) => {
     res.render('add_view')
 })
+app.post('/save', (req, res) => {
+    db.users.insert({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        tel: req.body.tel
+    }, (err, data) => {
+        res.redirect('/')
+    })
 
-app.listen('3000',()=>{
+})
+app.listen('3000', () => {
     console.log("Listening server on 3000");
 })
